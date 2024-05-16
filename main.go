2abc08d7357a4ec82424bbb977a2e2e2cdc02fa9
Ruben Claessens <78033666+DrTuup@users.noreply.github.com>
2024-05-16 16:02:46 +0200

    dockerize

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"rubenclaessens.nl/kilometerweter-backend/config"
@@ -9,11 +14,25 @@ import (
 )
 
 func main() {
+	godotenv.Load()
+	DB_HOST := os.Getenv("DB_HOST")
+	DB_USERNAME := os.Getenv("DB_USERNAME")
+	DB_PASSWORD := os.Getenv("DB_PASSWORD")
+	DB_NAME := os.Getenv("DB_NAME")
+	DB_PORT := os.Getenv("DB_PORT")
+	APP_PORT := os.Getenv("APP_PORT")
+	GIN_MODE := os.Getenv("GIN_MODE")
+
+	if GIN_MODE == "release" {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
 	// Connect and migrate database
-	config.DB, _ = gorm.Open(postgres.Open("host=localhost user=postgres password=postgres dbname=kilometerweter port=5432"), &gorm.Config{})
+	config.DB, _ = gorm.Open(postgres.Open(fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT)), &gorm.Config{})
 	config.DB.AutoMigrate(&models.Registration{})
 
 	r := routers.SetupRouters()
 
-	r.Run(":8080")
+	r.Run(":" + APP_PORT)
+
 }
